02f8a831ccbd9d4ce853f9dd6833a006b2d48acf
TeamDlxx <52232678+TeamDlxx@users.noreply.github.com>
2021-04-27 22:45:41 +0500

    Update s3.env.js

--- config/S3_config/s3.env.js
+++ config/S3_config/s3.env.js
@@ -1,8 +1,1 @@
-const env = {
-  AWS_ACCESS_KEY: "AKIAV5DSR3QRTKTYZGPF",
-  AWS_SECRET_ACCESS_KEY: "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",
-  REGION: "us-east-2",
-  Bucket: "evom-dev-storage",
-};
-
-module.exports = env;
+ 
